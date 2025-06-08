da631fef0662c0b3d0760cf9df129d2f8162d424
LeonardoB-07 <leonardocbaratela@gmail.com>
2025-06-08 17:11:47 -0300

    Update p2.c

---
+++ src/p2.c
@@ -0,0 +1,15 @@
+#include "list.h"
+#include "mil_tree.h"
+
+void buildMileageTree(List* carList) {
+    TreeNode root = NULL;
+    Node* current = carList->head;
+    while (current != NULL) {
+        root = insertNode(root, current->car->mileage, current->car);
+        current = current->next;
+    }
+
+    printf("Árvore de quilometragem criada.\n");
+
+    freeTree(root);
+}
